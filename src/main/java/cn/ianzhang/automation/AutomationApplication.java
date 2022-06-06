aba73920bc78a22cf0d92f75b7d234e2b80e12e3
ian.zhang <ian.zhangzhe@gmail.com>
2022-06-06 21:14:48 +0800

    AT-2 Upgrade Dependencies

--- src/main/java/cn/ianzhang/automation/AutomationApplication.java
+++ src/main/java/cn/ianzhang/automation/AutomationApplication.java
@@ -3,6 +3,9 @@ package cn.ianzhang.automation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Ian
+ */
 @SpringBootApplication
 public class AutomationApplication {
 
