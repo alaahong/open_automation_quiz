cc22d9b42dacc2ee32717ab63f9d03c06447f272
ian zhang <ian.zhangzhe@gmail.com>
2025-07-24 22:01:25 +0800

    chore: upgrade version and involve code quality

--- src/test/java/cn/ianzhang/automation/AutomationApplicationTest.java
+++ src/test/java/cn/ianzhang/automation/AutomationApplicationTest.java
@@ -8,16 +8,15 @@ import org.springframework.boot.SpringApplication;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class AutomationApplicationTest {
+    
     @Test
     void testMainMethod() {
         try (MockedStatic<SpringApplication> mockedSpringApplication = Mockito.mockStatic(SpringApplication.class)) {
-            AutomationApplication.main(new String[]{});
-            mockedSpringApplication.verify(() -> 
-                SpringApplication.run(AutomationApplication.class, new String[]{})
-            );
+            AutomationApplication.main(new String[] {});
+            mockedSpringApplication.verify(() -> SpringApplication.run(AutomationApplication.class, new String[] {}));
         }
     }
-
+    
     @Test
     void testMainMethodWithArgs() {
         try (MockedStatic<SpringApplication> mockedSpringApplication = Mockito.mockStatic(SpringApplication.class)) {
@@ -26,11 +25,9 @@ class AutomationApplicationTest {
             mockedSpringApplication.verify(() -> SpringApplication.run(AutomationApplication.class, args));
         }
     }
-
+    
     @Test
     void testClassInitialization() {
-        assertDoesNotThrow(() -> {
-            new AutomationApplication();
-        });
+        assertDoesNotThrow(AutomationApplication::new);
     }
 }
