2f16f97cc0930fa23e35e7a8da972217b65a7d9d
ian zhang <ian.zhangzhe@gmail.com>
2025-10-26 16:19:18 +0800

    feat: Add TestUtils class and corresponding unit tests for positive verification

---
+++ src/main/java/cn/ianzhang/automation/utils/TestUtils.java
@@ -0,0 +1,8 @@
+package cn.ianzhang.automation.utils;
+
+public class TestUtils {
+
+    public static boolean positiveVerification(int number){
+        return number > 0;
+    }
+}
---
+++ src/test/java/cn/ianzhang/automation/utils/TestUtilsTest.java
@@ -0,0 +1,54 @@
+package cn.ianzhang.automation.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TestUtilsTest {
+
+    @Test
+    void positiveVerificationReturnsTrueForPositiveNumber() {
+        assertTrue(TestUtils.positiveVerification(5));
+    }
+
+    @Test
+    void positiveVerificationReturnsFalseForZero() {
+        assertFalse(TestUtils.positiveVerification(0));
+    }
+
+    @Test
+    void positiveVerificationReturnsFalseForNegativeNumber() {
+        assertFalse(TestUtils.positiveVerification(-3));
+    }
+
+    @Test
+    void positiveVerificationReturnsTrueForLargePositiveNumber() {
+        assertTrue(TestUtils.positiveVerification(1000000));
+    }
+
+    @Test
+    void positiveVerificationReturnsTrueForLargePositiveNumberFailure() {
+        assertTrue(TestUtils.positiveVerification(-1));
+    }
+
+    @Test
+    void positiveVerificationReturnsFalseForNegativeBoundary() {
+        assertFalse(TestUtils.positiveVerification(-1));
+    }
+
+    @Test
+    void positiveVerificationReturnsTrueForSmallestPositiveNumber() {
+        assertTrue(TestUtils.positiveVerification(1));
+    }
+
+    @Test
+    void positiveVerificationReturnsFalseForIntegerMinValue() {
+        assertFalse(TestUtils.positiveVerification(Integer.MIN_VALUE));
+    }
+
+    @Test
+    void positiveVerificationReturnsTrueForIntegerMaxValue() {
+        assertTrue(TestUtils.positiveVerification(Integer.MAX_VALUE));
+    }
+}
